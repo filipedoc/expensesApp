c9ac3e4b595d4e9cf3d567f8485aeb2d5ab98477
filipedoc <lfocsgs@hotmail.com>
2024-04-23 20:48:39 -0300

    Setup Dagger-Hilt

--- app/src/main/java/br/com/expensesapp/ExpensesApplication.kt
+++ app/src/main/java/br/com/expensesapp/ExpensesApplication.kt
@@ -1,4 +1,7 @@
 package br.com.expensesapp
 
-class ExpensesApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExpensesApplication: Application()
--- app/src/main/java/br/com/expensesapp/di/DataBaseModule.kt
+++ app/src/main/java/br/com/expensesapp/di/DataBaseModule.kt
@@ -1,4 +1,37 @@
 package br.com.expensesapp.di
 
-class DataBaseModule {
+import android.content.Context
+import androidx.room.Room
+import br.com.expensesapp.data.CollectionDao
+import br.com.expensesapp.data.ExpenseDao
+import br.com.expensesapp.data.MyRoomDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        MyRoomDatabase::class.java,
+        "Expenses.db"
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideCollectionDao(database: MyRoomDatabase): CollectionDao = database.collectionDao()
+
+    @Singleton
+    @Provides
+    fun provideExpenseDao(database: MyRoomDatabase): ExpenseDao = database.expenseDao()
+
 }
